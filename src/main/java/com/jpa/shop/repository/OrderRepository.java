a221347f3df7f7bd9ecab189cc7e8c4bdecae011
YuHyeon <klyhyeon@gmail.com>
2021-08-14 06:54:32 +0900

    feat: 회원별 주문검색하기 작성중

--- src/main/java/com/jpa/shop/repository/OrderRepository.java
+++ src/main/java/com/jpa/shop/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.jpa.shop.repository;
 
 import com.jpa.shop.domain.Order;
+import com.jpa.shop.domain.OrderSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    List<Order> findAll(OrderSearch orderSearch);
 }
