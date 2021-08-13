3d5b478362860a5034c63abca97f83d60e444e48
YuHyeon <klyhyeon@globalorder.co.kr>
2021-08-13 15:50:27 +0900

    chore: @Getter 어노테이션 추가

--- src/main/java/com/jpa/shop/domain/BaseEntity.java
+++ src/main/java/com/jpa/shop/domain/BaseEntity.java
@@ -1,8 +1,11 @@
 package com.jpa.shop.domain;
 
+import lombok.Getter;
+
 import javax.persistence.*;
 
 @MappedSuperclass
+@Getter
 public class BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
